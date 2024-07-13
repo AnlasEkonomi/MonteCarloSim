503db2074da684540d44d680cb28f0afb412d1f8
Anlaşılır Ekonomi <anlasilirekonomi@gmail.com>
2024-07-13 15:44:44 +0300

    Update Monte_Carlo_Sim.py

--- Monte_Carlo_Sim.py
+++ Monte_Carlo_Sim.py
@@ -41,14 +41,14 @@ simülasyon=mc(ilkfiyat,ort_getiri,standart_sapma,deneme_sayısı,gün_sayısı)
 
 #Güven Aralıkları ve Ortalama
 alt_sınır=np.percentile(simülasyon,2.5,axis=0)
-upper_bound = np.percentile(simülasyon,97.5,axis=0)
+ust_sınır = np.percentile(simülasyon,97.5,axis=0)
 sim_ort=np.mean(simülasyon,axis=0)
 
 #Görselleştirme
 plt.plot(simülasyon.T,alpha=0.1,color="blue")
 plt.plot(sim_ort,color="red",label="Ortalama Simülasyon")
 plt.plot(alt_sınır,color="black",linestyle="--",label="%95 Alt Güven Aralığı",linewidth=2.2)
-plt.plot(upper_bound,color="black",linestyle="--",label="%95 Üst Güven Aralığı",linewidth=2.2)
+plt.plot(ust_sınır,color="black",linestyle="--",label="%95 Üst Güven Aralığı",linewidth=2.2)
 plt.title(f'{hisse_senedi} İçin Monte Carlo Simülasyonu (Deneme Sayısı:{deneme_sayısı})')
 plt.xlabel("Gün Sayısı")
 plt.ylabel("Fiyat")
